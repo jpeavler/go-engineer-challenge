2fff17b3ceb8e5118f5e2fdaf91172d10f9a2c1b
Joshua Peavler <joshuaapeavler@gmail.com>
2022-05-09 17:39:14 -0600

    Setup fetch data javascript and added basic jQuery to display basic ship data.

---
+++ js/fetch-data.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $.get("https://swapi.dev/api/starships/9", function(response) {
+        const data = response;
+        console.log(data);
+        $("#ship-name").text(data?.name);
+        $("#ship-model").text(data?.model);
+        $('#ship-manufacturer').text(`Manufactured by: ${data?.manufacturer}`);
+        $('#ship-cost').text(`Cost: ${data?.cost_in_credits} credits`);
+        $('#length').text(`Length: ${data?.length}`);
+        $('#max-atmosphering-speed').text(`Maximumn Atmosphering Speed: ${data?.max_atmosphering_speed}`);
+        $("#ship-crew").text(`Crew size: ${data?.crew}`);
+        $("#ship-passengers").text(`Crew size: ${data?.passengers}`);
+        $("#ship-cargo-capacity").text(`Cargo Capacity: ${data?.cargo_capacity}`);
+        $('#consumables').text(`Consumables: ${data?.consumables}`);
+        $('#hyperdrive-rating').text(`Hyperdrive Rating: ${data?.hyperdrive_rating}`);
+        $('#ship-mglt').text(`MGLT: ${data?.MGLT}`);
+        $('#starship-class').text(`MGLT: ${data?.starship_class}`);
+    });
+})
