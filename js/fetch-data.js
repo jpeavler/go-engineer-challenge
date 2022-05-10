d5139ca85163f3d9df583d3772c825e82095a09b
jpeavler <joshuaapeavler@gmail.com>
2022-05-10 13:10:38 -0600

    Fixed typos, added comments, and added formating of numbers for easier readability.

--- js/fetch-data.js
+++ js/fetch-data.js
@@ -1,5 +1,15 @@
-let allShips = [];
-let secondShipsArray = [];
+let allShips = [];  //Used to store all ships
+let secondShipsArray = [];  //Used to store all ships already randomly selected
+
+/**
+ * 
+ * @param {string} num: a string that represents a number to format 
+ * @returns a formated string of the number (adds commas) or just the string
+ * if the string is not a number.
+ */
+function formatNumber(num) {
+    return isNaN(num) ? num : parseInt(num).toLocaleString("en-US");
+}
 
 /**
  * function setShipDataInDom
@@ -9,23 +19,28 @@ let secondShipsArray = [];
 function setShipDataInDOM(ship) {
     $("#ship-name").text(ship?.name);
     $("#ship-model").text(ship?.model);
-    $('#ship-manufacturer').text(`Manufactured by: ${ship?.manufacturer}`);
-    $('#ship-cost').text(`Cost: ${ship?.cost_in_credits} Credits`);
-    $('#length').text(`Length: ${ship?.length}`);
-    $('#max-atmosphering-speed').text(`Maximumn Atmosphering Speed: ${ship?.max_atmosphering_speed}`);
-    $("#ship-crew").text(`Crew size: ${ship?.crew}`);
-    $("#ship-passengers").text(`Crew size: ${ship?.passengers}`);
-    $("#ship-cargo-capacity").text(`Cargo Capacity: ${ship?.cargo_capacity}`);
+    $('#ship-manufacturer').text(`Manufactured By: ${ship?.manufacturer}`);
+    $('#ship-cost').text(`Cost: ${formatNumber(ship?.cost_in_credits)} Credits`);
+    $('#length').text(`Length: ${ship?.length} Units`);
+    $('#max-atmosphering-speed').text(`Maximun Atmosphering Speed: ${formatNumber(ship?.max_atmosphering_speed)} Units`);
+    $("#ship-crew").text(`Crew size: ${formatNumber(ship?.crew)}`);
+    $("#ship-passengers").text(`Passenger Capacity: ${formatNumber(ship?.passengers)}`);
+    $("#ship-cargo-capacity").text(`Cargo Capacity: ${formatNumber(ship?.cargo_capacity)}`);
     $('#consumables').text(`Consumables: ${ship?.consumables}`);
     $('#hyperdrive-rating').text(`Hyperdrive Rating: ${ship?.hyperdrive_rating}`);
-    $('#ship-mglt').text(`MGLT: ${ship?.MGLT}`);
+    $('#ship-mglt').text(`MGLT: ${formatNumber(ship?.MGLT)}`);
     $('#starship-class').text(`Starship Class: ${ship?.starship_class}`);
 }
 
+/**
+ * function getShips
+ * @returns an array of all Star Wars starships from the swapi api
+ */
 async function getShips() {
     let ships = [];
     let starshipsEndpoint = "https://swapi.dev/api/starships";
 
+    //Handles pagination of star ships
     while(starshipsEndpoint) {
         let response = await $.get(starshipsEndpoint);
         ships.push(response?.results);
@@ -34,12 +49,18 @@ async function getShips() {
     return ships.flat();
 }
 
+/**
+ * function getRandomShip
+ * @returns an object describing one of the Star Wars star ships
+ */
 function getRandomShip() {
+    //If all of the ships have been shown off, reset the deck
     if(allShips.length === 0) {
         allShips.push(... secondShipsArray);
         secondShipsArray = [];
     }
 
+    //Pick a random ship and move it from the allShips array to the secondShipsArray
     const randomIndex = Math.floor(Math.random() * allShips.length);
     const randomShip = allShips[randomIndex];
     allShips.splice(randomIndex, 1);
