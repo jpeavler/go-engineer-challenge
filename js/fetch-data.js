15b76b4c4326877df58e21bcda410e459edb1e48
Joshua Peavler <joshuaapeavler@gmail.com>
2022-05-09 20:30:52 -0600

    Fixed typos.

--- js/fetch-data.js
+++ js/fetch-data.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         $("#ship-name").text(data?.name);
         $("#ship-model").text(data?.model);
         $('#ship-manufacturer').text(`Manufactured by: ${data?.manufacturer}`);
-        $('#ship-cost').text(`Cost: ${data?.cost_in_credits} credits`);
+        $('#ship-cost').text(`Cost: ${data?.cost_in_credits} Credits`);
         $('#length').text(`Length: ${data?.length}`);
         $('#max-atmosphering-speed').text(`Maximumn Atmosphering Speed: ${data?.max_atmosphering_speed}`);
         $("#ship-crew").text(`Crew size: ${data?.crew}`);
@@ -14,6 +14,6 @@ $(document).ready(function() {
         $('#consumables').text(`Consumables: ${data?.consumables}`);
         $('#hyperdrive-rating').text(`Hyperdrive Rating: ${data?.hyperdrive_rating}`);
         $('#ship-mglt').text(`MGLT: ${data?.MGLT}`);
-        $('#starship-class').text(`MGLT: ${data?.starship_class}`);
+        $('#starship-class').text(`Starship Class: ${data?.starship_class}`);
     });
 })
