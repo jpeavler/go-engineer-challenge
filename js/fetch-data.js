f0631c910f7c477942e333358b8c7b5196975819
jpeavler <joshuaapeavler@gmail.com>
2022-05-09 23:08:18 -0600

    Implemented full collection of ship data and setup display of random ship from list every ten seconds.

--- js/fetch-data.js
+++ js/fetch-data.js
@@ -1,19 +1,59 @@
+let allShips = [];
+
+/**
+ * function setShipDataInDom
+ * @param {object} ship: an object describing the ship.
+ * This function inserts ship data into the DOM. 
+ */
+function setShipDataInDOM(ship) {
+    $("#ship-name").text(ship?.name);
+    $("#ship-model").text(ship?.model);
+    $('#ship-manufacturer').text(`Manufactured by: ${ship?.manufacturer}`);
+    $('#ship-cost').text(`Cost: ${ship?.cost_in_credits} Credits`);
+    $('#length').text(`Length: ${ship?.length}`);
+    $('#max-atmosphering-speed').text(`Maximumn Atmosphering Speed: ${ship?.max_atmosphering_speed}`);
+    $("#ship-crew").text(`Crew size: ${ship?.crew}`);
+    $("#ship-passengers").text(`Crew size: ${ship?.passengers}`);
+    $("#ship-cargo-capacity").text(`Cargo Capacity: ${ship?.cargo_capacity}`);
+    $('#consumables').text(`Consumables: ${ship?.consumables}`);
+    $('#hyperdrive-rating').text(`Hyperdrive Rating: ${ship?.hyperdrive_rating}`);
+    $('#ship-mglt').text(`MGLT: ${ship?.MGLT}`);
+    $('#starship-class').text(`Starship Class: ${ship?.starship_class}`);
+}
+
+async function getShips() {
+    let ships = [];
+    let starshipsEndpoint = "https://swapi.dev/api/starships";
+
+    while(starshipsEndpoint) {
+        console.log("while loop entered")
+        let response = await $.get(starshipsEndpoint);
+        console.log("My response", response.results);
+        ships.push(response?.results);
+        starshipsEndpoint = response?.next;
+    }
+    return ships.flat();
+}
+
+function getRandomShip(shipArray) {
+    const randomIndex = Math.floor(Math.random() * shipArray.length);
+    const randomShip = shipArray[randomIndex];
+
+    return randomShip;
+}
+
 $(document).ready(function() {
-    $.get("https://swapi.dev/api/starships/9", function(response) {
-        const data = response;
-        console.log(data);
-        $("#ship-name").text(data?.name);
-        $("#ship-model").text(data?.model);
-        $('#ship-manufacturer').text(`Manufactured by: ${data?.manufacturer}`);
-        $('#ship-cost').text(`Cost: ${data?.cost_in_credits} Credits`);
-        $('#length').text(`Length: ${data?.length}`);
-        $('#max-atmosphering-speed').text(`Maximumn Atmosphering Speed: ${data?.max_atmosphering_speed}`);
-        $("#ship-crew").text(`Crew size: ${data?.crew}`);
-        $("#ship-passengers").text(`Crew size: ${data?.passengers}`);
-        $("#ship-cargo-capacity").text(`Cargo Capacity: ${data?.cargo_capacity}`);
-        $('#consumables').text(`Consumables: ${data?.consumables}`);
-        $('#hyperdrive-rating').text(`Hyperdrive Rating: ${data?.hyperdrive_rating}`);
-        $('#ship-mglt').text(`MGLT: ${data?.MGLT}`);
-        $('#starship-class').text(`Starship Class: ${data?.starship_class}`);
-    });
-})
+    getShips()
+        .then(ships => {
+            allShips = ships;
+            console.log("My ships", allShips);
+            const randomShip = getRandomShip(allShips);
+            setShipDataInDOM(randomShip);
+        });
+});
+
+setInterval(function() {
+    console.log("Interval hit");
+    const randomShip = getRandomShip(allShips);
+    setShipDataInDOM(randomShip);
+}, 10000);
